c5179724033248a082078a35c937c95aa12af674
lautaro garay <garaylautaroe10@gmail.com>
2025-06-30 12:13:01 -0300

    add test

---
+++ src/test/java/org/oauth/controller/ProductoControllerTest.java
@@ -0,0 +1,24 @@
+package org.oauth.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductoControllerTest {
+
+    @Test
+    void listarTodos() {
+    }
+
+    @Test
+    void crearProducto() {
+    }
+
+    @Test
+    void actualizarProducto() {
+    }
+
+    @Test
+    void eliminarProducto() {
+    }
+}
